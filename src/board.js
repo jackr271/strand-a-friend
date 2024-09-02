9c541c1f893ed4dee7e8f6de54f7d10e6c01ac49
Jack Roche <52017135+jackr271@users.noreply.github.com>
2024-09-02 10:00:45 -0400

    refactor: add OPEN-checking to getNeighbors func

--- src/board.js
+++ src/board.js
@@ -13,6 +13,19 @@ export default class Board {
     }
 
 
+    // placeWord(word, span) {
+    //     const starts = randomize(this.available);
+
+    //     for (start of starts) {
+    //         this.#placeWord(start, subWord);
+
+    //     }
+    // }
+
+    // #placeWord(space, subWord) {
+
+    // }
+
     getSubBoards() {
         let subBoards = [];
         for (let i = 0; i < MAXROW; i++) {
@@ -34,13 +47,13 @@ export default class Board {
         while (current.length > 0) {
             let upNext = [];
             for (let coord of current) {
-                const neighbors = getNeighbors(coord[0], coord[1]);
+                const neighbors = this.#getNeighbors(coord[0], coord[1]);
                 for (let neighbor of neighbors) {
-                    if (this.spaces[neighbor[0]][neighbor[1]] === OPEN) {
-                        upNext.push(neighbor)
-                        this.spaces[neighbor[0]][neighbor[1]] = TAKEN;
-                        available.push(neighbor);
-                    }
+                    // if (this.spaces[neighbor[0]][neighbor[1]] === OPEN) {
+                    upNext.push(neighbor)
+                    this.spaces[neighbor[0]][neighbor[1]] = TAKEN;
+                    available.push(neighbor);
+                    // }
                 }
             }
             current = upNext;
@@ -58,28 +71,35 @@ export default class Board {
         }
         console.log(string);
     }
+
+    #getNeighbors(i, j) {
+        let neighbors = [];
+        if ((j - 1 >= 0) && (this.spaces[i][j - 1] === OPEN))
+            neighbors.push([i, j - 1])
+        if ((j + 1 < MAXCOL) && (this.spaces[i][j + 1] === OPEN))
+            neighbors.push([i, j + 1])
+        if (i - 1 >= 0) {
+            if (this.spaces[i - 1][j] === OPEN)
+                neighbors.push([i - 1, j])
+            if ((j - 1 >= 0) && (this.spaces[i - 1][j - 1] === OPEN))
+                neighbors.push([i - 1, j - 1])
+            if ((j + 1 < MAXCOL) && (this.spaces[i - 1][j + 1] === OPEN))
+                neighbors.push([i - 1, j + 1])
+        }
+        if (i + 1 < MAXROW) {
+            if (this.spaces[i + 1][j] === OPEN)
+                neighbors.push([i + 1, j])
+            if ((j - 1 >= 0) && (this.spaces[i + 1][j - 1] === OPEN))
+                neighbors.push([i + 1, j - 1])
+            if ((j + 1 < MAXCOL) && (this.spaces[i + 1][j + 1] === OPEN))
+                neighbors.push([i + 1, j + 1])
+        }
+        return neighbors;
+    }
 }
 
-
-function getNeighbors(i, j) {
-    let neighbors = [];
-    if (j - 1 >= 0)
-        neighbors.push([i, j - 1])
-    if (j + 1 < MAXCOL)
-        neighbors.push([i, j + 1])
-    if (i - 1 >= 0) {
-        neighbors.push([i - 1, j])
-        if (j - 1 >= 0)
-            neighbors.push([i - 1, j - 1])
-        if (j + 1 < MAXCOL)
-            neighbors.push([i - 1, j + 1])
-    }
-    if (i + 1 < MAXROW) {
-        neighbors.push([i + 1, j])
-        if (j - 1 >= 0)
-            neighbors.push([i + 1, j - 1])
-        if (j + 1 < MAXCOL)
-            neighbors.push([i + 1, j + 1])
-    }
-    return neighbors;
+function randomize(arr) {
+    const randomStart = Math.floor(Math.random() * arr.length);
+    let newArr = arr.slice(randomStart).concat(arr.slice(0, randomStart));
+    return newArr;
 }
